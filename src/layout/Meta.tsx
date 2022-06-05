4a9c6b6837362102c9e96c01711a8e7820e54f3e
Jake Valenzuela <jakev224@gmail.com>
2022-06-05 17:14:17 -0400

    Cleaning up frontend

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -18,22 +18,11 @@ const Meta = (props: IMetaProps) => (
         content="width=device-width,initial-scale=1"
         key="viewport"
       />
-      <link rel="apple-touch-icon" href="/apple-touch-icon.png" key="apple" />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-        key="icon32"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-        key="icon16"
-      />
-      <link rel="icon" href="/favicon.ico" key="favicon" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="dark/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="dark/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="dark/favicon-16x16.png" />
+      <link rel="manifest" href="dark/site.webmanifest" />
     </Head>
     <NextSeo
       title={props.title}
